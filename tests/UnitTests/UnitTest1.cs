1ab10b10ad436588ebba35ef4edba524056935de
Vyshnavi Thannir <s531441@nwmissouri.edu>
2019-02-08 16:10:32 -0600

    Unit Test cases

--- tests/UnitTests/UnitTest1.cs
+++ tests/UnitTests/UnitTest1.cs
@@ -8,7 +8,22 @@ namespace UnitTests
         [Fact]
         public void Test1()
         {
+            var result = 4;
+            var x = 16;
 
+            Assert.Equal(4, SquareRoot(16));
+
+        }
+
+        [Theory]
+        [InlineData(25, 5)]
+        [InlineData(0, 0)]
+
+        public void Test2(int test, int a) => Assert.Equal(a,SquareRoot(test));
+
+        double SquareRoot(int a )
+        {
+            return Math.Sqrt(a);
         }
     }
 }
